379b6ace7281781f41825d420ed7a5ee5519f551
Christopher Rehm <chrisotpherrehm@web.de>
2019-12-15 10:38:56 +0100

    added some code

---
+++ firstLevelProcessor.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+#first level message processing program. this processes incoming data from sensors, visual, audio, sonar etc
+
+# by christopher Rehm. 15 dec 2019
+
+run = True
+
+while (run = True)
+    #check for incoming new message
+
+
+     #if its a kill message, set run level to false and break out of loop, kill program. 
+
+
+    # select a processing program and a location to run  process
+
+    #send off command to do process 
+
+    #move message to processed folder
+
+     #do it again
+	
