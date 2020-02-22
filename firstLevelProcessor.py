6f596e3ea3ee942934be37ed7afa31ba5a1b4c4d
chris Rehm <christopherrehm@web.de>
2020-02-23 00:56:34 +0100

    async message passing and 100% processing no losses

--- firstLevelProcessor.py
+++ firstLevelProcessor.py
@@ -5,43 +5,50 @@
 # by christopher Rehm. 15 dec 2019
 
 import os
-import json
+import sys
+import time
 import grendelconfig as gc
 
+def makeMsg(title, text, primeRecipient, priority, otherRecievers, files):
+    mytime = time.time()
+    mymessage = gc.message()
+    mymessage.write(mytime, title, text,primeRecipient, priority, "PY", otherRecievers, files)
+
+
+firsttime =  True
 run = True
 while (run == True):
     print("starting message handling loop now")
+    gc.debugBreakPoint("PY1")
     #check for incoming new message
     newMsgs = os.listdir(gc.msgPathPY)
     #print(newMsgs)
     #print ('***********')
+    if firsttime == True:
+        makeMsg("Py startup","starting py program","AI", "3" ,"NOONE", "NONE")
+        firsttime = False
     for each in newMsgs:
-        if os.path.isfile(gc.msgPathPY + "/" + each):
-            ##need method to check for new messages here, if none loop
-            with open(gc.msgPathPY + "/" + each) as f:
-                msgData =json.load(f)
-            #if its a kill message, set run level to false and break out of loop, kill program.
-            if msgData[0] == "kill":
-                run = False
-                break
-            # select a processing program and a location to run  process
-            elif msgData[0] == "newfoto":
-                #print(msgDataPiece)
-                print("sending new foto data for processing")
-                pass
-                #send off command to do process
-            elif msgData[0] == "newaudio":
-                print("sending new audio data for processing")
-                #send off command to do process
-            elif msgData[0] == "newtext":
-                print("sending new text data for processing")
-                #send off command to do process
-            else:
-                print("I don't know how to process that data")
-                print(each)
-            #move message to processed folder
-            os.system('mv ' + gc.msgPathPY + "/" + each + ' ' + gc.msgPath + '/processedMsgs/')
-            #do it again
+        mymessage = gc.message()
+        mymessage.read(each,"PY")
+        # select a processing program and a location to run  process
+        if mymessage[1] == "newfoto":
+            #print(msgDataPiece)
+            print("sending new foto data for processing")
+            #send off command to do process
+        elif mymessage[1] == "newaudio":
+            print("sending new audio data for processing")
+            #send off command to do process
+        elif mymessage[1] == "newtext":
+            print("sending new text data for processing")
+            #send off command to do process
+        elif mymessage[1] == "shutdownGrendel":
+            #save any data
+            sys.exit()
         else:
-            pass
-            #do nothing, loop and wait for more messages
+            print("I don't know how to process that data")
+            #print(mymessage)
+            makeMsg("testttttt","arggggg blahblahblah and blah","AI", "!" ,"NOONE", "NONE")
+            gc.debugBreakPoint("PY2")
+        #move message to processed folder
+        os.system('mv ' + gc.msgPathPY + "/" + each + ' ' + gc.msgPath + '/processedMsgs/')
+    #do it again
